c1c979a04fa4f665bf5e9b9c75c61122604fbbbc
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2021-04-22 08:18:58 -0700

    Completed Participation 9

---
+++ Participation Assignments/Q9/name_age.js
@@ -0,0 +1,4 @@
+let name = prompt("What it your name?", "[ENTER NAME HERE]");
+let age = prompt("What is your age?", "[ENTER YOUR AGE HERE]");
+
+alert(name + ", this year, you turn/have turned " + age + " years old.");
