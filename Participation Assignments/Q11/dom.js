a30d610cf6167e20debae72247c66e4fe314edde
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2021-04-29 05:36:25 -0700

    Added comments to the JS code

--- Participation Assignments/Q11/dom.js
+++ Participation Assignments/Q11/dom.js
@@ -1,24 +1,27 @@
-let num = 0;
+let num = 0; //number keeps track how many times the button has been pressed
 
+// Keeps track of whether the current button click was odd or even
 function EvenOrOdd() {
   if (num % 2 === 0) {
     return num - 1;
   } else return;
 }
 
-const button = document.getElementById("button");
+const button = document.getElementById("button"); //selects button with its ID
 
+// onclick event controls the list that is displayed
 button.onclick = () => {
-  let line = "";
+  let line = ""; //set the like variable to empty
 
-  const ordered_list = document.getElementById("list");
-  const text_input = document.getElementById("text-input");
+  const ordered_list = document.getElementById("list"); //selects ordered list with its ID
+  const text_input = document.getElementById("text-input"); //selects the text input by its ID
 
-  num++;
-  line = "<li/>" + text_input.value;
-  ordered_list.innerHTML += line;
-  text_input.value = "";
+  num++; //adds +1 to the variable for each onclick event
+  line = "<li/>" + text_input.value; //sets the line equal to a new list item with the text input inline
+  ordered_list.innerHTML += line; //adds new list item to the ordered list
+  text_input.value = ""; //clears the input text
 
+  //add styling to every even entry in the ordered list
   document
     .getElementsByTagName("li")
     [EvenOrOdd()].setAttribute("class", "even");
