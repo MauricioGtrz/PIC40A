4cf3c11a180399e389e7e1373410ec13c8a94876
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2021-04-29 04:21:06 -0700

    Cleaned up some of the JS. Added new variable that selects the text input

--- Participation Assignments/Q11/dom.js
+++ Participation Assignments/Q11/dom.js
@@ -13,10 +13,13 @@ button.onclick = () => {
 
   const ordered_list = document.getElementById("list");
   const new_list_item = (ordered_list.innerHTML += line);
+  const text_input = document.getElementById("text-input");
 
   num++;
   console.log(num);
   alert(EvenOrOdd());
-  document.getElementById("text-input").value = "";
-  ordered_list.appendChild(new_list_item);
+  line = text_input.value + "<li/>";
+  console.log(line);
+  new_list_item;
+  text_input.value = "";
 };
