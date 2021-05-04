998edef1417df3b7c5e71337ee3e85880f3aefe2
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2021-05-03 23:09:59 -0700

    Linked the js file to the html and added a span to the results with the results id. Added js logic to obtain values of the firs and second inputs

---
+++ Homework Assignments/HW3/app.js
@@ -0,0 +1,8 @@
+alert("This is a JavaScript calculator");
+
+document.getElementById("compute").addEventListener("click", function () {
+  let first_value = document.getElementById("first-value").value;
+  let second_value = document.getElementById("second-value").value;
+
+  document.getElementById("result").innerHTML = "hello";
+});
