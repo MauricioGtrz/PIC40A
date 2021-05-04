26348cd317d9e3658059f4dfbf3f92c4620f743a
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2021-05-04 00:10:29 -0700

    Finished the Homework assignment

--- Homework Assignments/HW3/app.js
+++ Homework Assignments/HW3/app.js
@@ -4,5 +4,24 @@ document.getElementById("compute").addEventListener("click", function () {
   let first_value = document.getElementById("first-value").value;
   let second_value = document.getElementById("second-value").value;
 
-  document.getElementById("result").innerHTML = "hello";
+  function set_result(x) {
+    document.getElementById("result").innerHTML = x;
+  }
+  result = "no";
+  let add = document.getElementById("add");
+  let subtract = document.getElementById("subtract");
+  let multiply = document.getElementById("multiply");
+  let divide = document.getElementById("divide");
+
+  if (add.checked === true) {
+    set_result(first_value * 1 + second_value * 1);
+  } else if (subtract.checked === true) {
+    set_result(first_value * 1 - second_value * 1);
+  } else if (multiply.checked === true) {
+    set_result(first_value * 1 * (second_value * 1));
+  } else if (divide.checked === true) {
+    set_result((first_value * 1) / (second_value * 1));
+  } else {
+    alert("Something went terribly wrong!");
+  }
 });
