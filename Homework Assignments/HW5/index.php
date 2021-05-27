c9916c7065e9f45c640e33f55a23cb6fce6fb771
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-05-26 21:56:58 -0700

    Added css file that adds borders to the table. Finished php hailstone sequence and output to table.

--- Homework Assignments/HW5/index.php
+++ Homework Assignments/HW5/index.php
@@ -5,26 +5,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>TABLE OF HAILSTONE SEQUENCE VALUES</title>
 </head>
 <body>
-<table>
-      <thead>
-        <tr>
-          <th>Iteration</th>
-          <th>x</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>0</td>
-          <td>5</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>16</td>
-        </tr>
-      </tbody>
-    </table>
+  <table>
+    <thead>
+      <tr>
+        <th>Iteration</th>
+        <th>x</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php 
+    $x = $_GET['x'];
+    $i = 0;
+    function hailstoneSequence($n, $iteration) {
+      if ($n === 1) {?>
+        <tr> <td><?php echo $iteration?></td> <td><?php echo $n?></td> </tr>
+        <?php
+        return;
+      }
+      ?>
+      <tr> <td><?php echo $iteration?></td> <td><?php echo $n?></td> </tr>
+      <?php
+      $iteration++;
+      if ($n % 2 === 0) {
+        hailstoneSequence($n / 2, $iteration);
+      } else {
+        hailstoneSequence(3 * $n + 1, $iteration);
+      }
+    }
+
+    hailstoneSequence($x, $i);
+  ?>
+    </tbody>
+  </table>
 </body>
 </html>
