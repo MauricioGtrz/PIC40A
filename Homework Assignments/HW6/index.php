9900607da25f12a6d1581b9ce513f253cc338969
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-05-28 20:12:15 -0700

    Added sample login for server and password text

--- Homework Assignments/HW6/index.php
+++ Homework Assignments/HW6/index.php
@@ -1,18 +1,50 @@
 #!/usr/local/bin/php
 <?php
-  if( isset($_POST['add_php']) ){   
-    //$_COOKIE['type'] = 'php'; 
-    setcookie('type', '', time()-1 );
-    setcookie('type', 'php', time()+60);
-    header('Location: index.php');
-    exit;       
-  }
-  if( isset($_POST['remove_php']) ){
-    //unset( $_COOKIE['type'] );
-    setcookie('type', '', time()-1 );
-    header('Location: index.php');
-    exit;    
-  }  
+    session_save_path(dirname(realpath(__FILE__)) . '/sessions/');
+    session_name('LoggedinSession'); // name the session
+    session_start(); // start a session
+    $_SESSION['loggedin'] = false; // have not logged in
+    /**
+    This function validates a password and sets the $_SESSION token to true
+    if it is correct, logging them in and sending them to the welcome page.
+    Otherwise it flags $error as true.
+
+    @param string $password the password the user entered
+    @param boolean $error the error flag to possibly change
+    */
+    function validate($password, &$error){
+        $fin = fopen('password.txt', 'r'); // open file to read
+        $true_pass = fgets($fin); // get the line
+        fclose($fin); // close the file
+        $true_pass = trim($true_pass); // trim white space
+
+        if($password === $true_pass){ // if they match, great
+            $_SESSION['loggedin'] = true;
+            header('Location: welcome.php');
+        }
+        else { // bad password
+            $error = true;
+        }
+    }
+
+    $error = false;
+    if(isset($_POST['pass'])){ // if something was posted
+        validate($_POST['pass'], $error); // check it
+    }
+  
+    if( isset($_POST['add_php']) ){   //ensure was set
+        $_COOKIE['type'] = 'php'; 
+        setcookie('type', '', time()-1 );
+        setcookie('type', 'php', time()+60);
+        header('Location: index.php');
+        exit;       
+    }
+    if( isset($_POST['remove_php']) ){
+        unset( $_COOKIE['type'] );
+        setcookie('type', '', time()-1 );
+        header('Location: index.php');
+        exit;    
+    }  
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,6 +74,13 @@
     <form method="post" action="<?php echo $_SEVER['PHP_SELF']; ?>" >
         <input type="submit" name="add_php" value="Add with PHP"/>
         <input type="submit" name="remove_php" value="Remove with PHP"/>
+    </form>
+
+    <form method = "post" action ="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="password" name="pass" /> <input type="submit" value="log in" />
+        <?php if($error) { // wrong password ?>
+        <p>Invalid password!</p> <?php
+        } ?>
     </form>
 </body>
 </html>
