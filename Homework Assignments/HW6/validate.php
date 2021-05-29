b82ffb8fc5ec856233abcf858eca445901a3f9a4
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-05-28 18:43:56 -0700

    Added other php files that will serve as pages when the user is validated and logged in.

---
+++ Homework Assignments/HW6/validate.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>USER ACCOUNT LOGIN SYSTEM</title>
+</head>
+<body>
+    <p>You are now registered! Follow this link to login: </p>
+</body>
+</html>
