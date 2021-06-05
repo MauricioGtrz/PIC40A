9480ec8e890f8d469fe2c31e7fb96a7bd322216c
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-06-04 18:51:31 -0700

    upload.php now checks the uploads folder to see if file already exists

--- Homework Assignments/HW7/upload.php
+++ Homework Assignments/HW7/upload.php
@@ -6,19 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>IMAGE SHARING SITE</title>
+    <title><?php echo "Thank you, "." ".$_POST['user_name']."!"; ?></title>
 </head>
 <body>
     <?php
         if( isset($_POST['img_upload_submit']) ){ // they did submit
-            echo "Thank you ", $_POST['user_name'], " for uploading ", $_POST['img_title'];
+            // echo "Thank you ", $_POST['user_name'], " for uploading ", $_POST['img_title'];
             $fileType = explode('.', $_FILES['user_file']['name']);
-    
             $fileName = $_POST['img_title'].'.'.$fileType[1];
             $saveLocation = dirname(realpath(__FILE__)) . '/uploads/' . $fileName;
-            move_uploaded_file($_FILES['user_file']['tmp_name'], $saveLocation);
 
-            echo '<br /><img src="./uploads/'.$fileName.'" alt="Your Uploaded Image" class="uploaded_img">';
+            $dir = "./uploads";
+            $files = scandir($dir);
+            if (array_search($fileName, $files) === false) {
+                echo "Your image has been uploaded";
+                move_uploaded_file($_FILES['user_file']['tmp_name'], $saveLocation);
+                echo '<br /><img src="./uploads/'.$fileName.'" alt="Your Uploaded Image" class="uploaded_img">';    
+            } else{
+                echo "A photo named ".$fileName." already exists.";
+                return;
+            }
+
         }
         else{
             echo 'no file submitted';
