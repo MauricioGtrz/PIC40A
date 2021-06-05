bdf15600bf8b7e1ba7dd8e9ca21d353fb2ba951b
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-06-04 20:26:43 -0700

    upload.php can now save file as png to uploads folder

--- Homework Assignments/HW7/upload.php
+++ Homework Assignments/HW7/upload.php
@@ -10,20 +10,24 @@
 </head>
 <body>
     <?php
-        if( isset($_POST['img_upload_submit']) ){ // they did submit
+        if( isset($_POST['img_upload_submit'])){ // they did submit
             // echo "Thank you ", $_POST['user_name'], " for uploading ", $_POST['img_title'];
             $fileType = explode('.', $_FILES['user_file']['name']);
             $fileName = $_POST['img_title'].'.'.$fileType[1];
             $saveLocation = dirname(realpath(__FILE__)) . '/uploads/' . $fileName;
+            
+            $newFileName = $_POST['img_title'].'.png';
 
             $dir = "./uploads";
             $files = scandir($dir);
-            if (array_search($fileName, $files) === false) {
+            if (array_search($newFileName, $files) === false) {
                 echo "Your image has been uploaded";
                 move_uploaded_file($_FILES['user_file']['tmp_name'], $saveLocation);
-                echo '<br /><img src="./uploads/'.$fileName.'" alt="Your Uploaded Image" class="uploaded_img">';    
+                exec("mogrify -format png $saveLocation");
+                exec("rm -f ./uploads/".$fileName);
+                echo '<br /><img src="./uploads/'.$newFileName.'" alt="Your Uploaded Image" class="uploaded_img">';    
             } else{
-                echo "A photo named ".$fileName." already exists.";
+                echo "A photo named ".$newFileName." already exists.";
                 return;
             }
 
