68d2a59bc4eeb73328e828207086c8b47cc5284b
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-06-04 17:58:27 -0700

    upload.php now uploads in the same file format that the image was originally uploaded in

--- Homework Assignments/HW7/upload.php
+++ Homework Assignments/HW7/upload.php
@@ -13,8 +13,9 @@
     <?php
         if( isset($_POST['img_upload_submit']) ){ // they did submit
             echo "Thank you ", $_POST['user_name'], " for uploading ", $_POST['img_title'];
+            $fileType = explode('.', $_FILES['user_file']['name']);
     
-            $fileName = $_FILES['user_file']['name'];
+            $fileName = $_POST['img_title'].'.'.$fileType[1];
             $saveLocation = dirname(realpath(__FILE__)) . '/uploads/' . $fileName;
             move_uploaded_file($_FILES['user_file']['tmp_name'], $saveLocation);
 
