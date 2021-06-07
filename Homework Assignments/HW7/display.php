5575937f49cf36ea573ac51966bc011588b441c8
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-06-07 02:44:54 -0700

    Image views now displays the current amount of previous views of each image.

--- Homework Assignments/HW7/display.php
+++ Homework Assignments/HW7/display.php
@@ -21,9 +21,14 @@
                 while($row = $run->fetchArray()){ // while still a row to parse
                     echo '<img src="'.$row['file_location'].'" alt="'.$row['upload_date'].'" title="'.$row['upload_date'].'" class="uploaded_img">', '<br />';
                     echo "<b>".$row['file_name']." has ".$row['file_views']." view(s)."."</b>"."<br />";
+                    
+                    $file_name = $row['file_name'];
+                    $updated_file_views = $row['file_views'];
+                    $db->query("UPDATE uploads_table SET file_views= $updated_file_views WHERE user_name= '$userName' AND file_name= '$file_name';");
                     //echo $row['file_name'], '--', $row['user_name'], '--', $row['file_location'], '--', $row['file_views'], '--', $row['upload_date'], '<br/>'; // print all the data
                 }
             }
+            $db->close();
         ?>
     </main>
 </body>
