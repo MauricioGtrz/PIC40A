f85aaf856cc64fb8df54a5f59d6305ade8c2ce3c
Mauricio Gutierrez <mau@Mauricios-MBP.lan>
2021-06-07 23:59:31 -0700

    Last commit! Final changes to meet homework requirements including some comments for the all the files

--- Homework Assignments/HW7/app.js
+++ Homework Assignments/HW7/app.js
@@ -1,8 +1,10 @@
 let img = document.getElementsByTagName("img");
 for (var i = 0; i < img.length; i++) {
+  //add event listener to each img tag
   img[i].addEventListener("click", ajax);
 }
 
+//make ajax call to get file name and allow php photo download
 function ajax(element) {
   let id = element.target.id;
   const x = new XMLHttpRequest();
